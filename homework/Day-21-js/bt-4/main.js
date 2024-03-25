22e1cffca7bf092108d554061ae5693b2563802a
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-03-25 09:06:51 +0700

    btvn buổi 21

--- homework/Day-21-js/bt-4/main.js
+++ homework/Day-21-js/bt-4/main.js
@@ -1,4 +1,4 @@
-var dataArray = [
+var arr = [
     ["Tiêu đề bài viết 1", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente praesentium voluptates in voluptatem quisquam qui exercitationem. Voluptas culpa ut, a aliquam beatae nisi provident laudantium nulla ipsam tempora corrupti explicabo.", "https://fastly.picsum.photos/id/450/536/354.jpg?hmac=9m3LrKj4vt0wUGoG8Eqmbw1V0LCJ9VKjotelYl8kLA8"],
     ["Tiêu đề bài viết 2", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente praesentium voluptates in voluptatem quisquam qui exercitationem. Voluptas culpa ut, a aliquam beatae nisi provident laudantium nulla ipsam tempora corrupti explicabo.", "https://fastly.picsum.photos/id/450/536/354.jpg?hmac=9m3LrKj4vt0wUGoG8Eqmbw1V0LCJ9VKjotelYl8kLA8"],
     ["Tiêu đề bài viết 3", "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente praesentium voluptates in voluptatem quisquam qui exercitationem. Voluptas culpa ut, a aliquam beatae nisi provident laudantium nulla ipsam tempora corrupti explicabo.", "https://fastly.picsum.photos/id/450/536/354.jpg?hmac=9m3LrKj4vt0wUGoG8Eqmbw1V0LCJ9VKjotelYl8kLA8"]
@@ -6,32 +6,32 @@ var dataArray = [
 
 var html = "";
 
-dataArray.forEach(function(data, index) {
-    var contentOrder;
+for (var i = 0; i < arr.length; i++) {
+    var content;
 
-    if (index % 2 === 0) {
-        contentOrder = `
+    if (i % 2 === 0) {
+        content = `
             <div class="title">
-                <h1>${data[0]}</h1>
-                <span>${data[1]}</span>
+                <h1>${arr[i][0]}</h1>
+                <span>${arr[i][1]}</span>
             </div>
             <div class="image">
-                <img src="${data[2]}" alt="" />
+                <img src="${arr[i][2]}" alt="" />
             </div>
         `;
     } else {
-        contentOrder = `
+        content = `
             <div class="image">
-                <img src="${data[2]}" alt="" />
+                <img src="${arr[i][2]}" alt="" />
             </div>
             <div class="title">
-                <h1>${data[0]}</h1>
-                <span>${data[1]}</span>
+                <h1>${arr[i][0]}</h1>
+                <span>${arr[i][1]}</span>
             </div>
         `;
     }
 
-    html += `<div class="box">${contentOrder}</div>`;
-});
+    html += `<div class="box">${content}</div>`;
+}
 
-document.write(html)
+document.write(html);
