71f88f3340aadd3138754479bd8d0dca1fdfcc5f
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-04-01 00:01:47 +0700

    Nộp Bài Tập Về Nhà Buổi 23

--- homework/Day-23-js/bt-1/main.js
+++ homework/Day-23-js/bt-1/main.js
@@ -1,56 +1,34 @@
-Viết 1 hàm trả về 1 đối tượng có 3 thuộc tính: name, password và email.
-
-Tạo một hàm register nhận vào nhiều tham số để khởi tạo ra một mảng chứa các đối tượng có cấu trúc như trên.
-
-Yêu cầu:
-
-Kiểm tra tất cả thông tin có đầy đủ không, nếu không đủ, báo lỗi và dừng chương trình.
-
-Nếu đăng ký thêm một lần nữa, phải trả về được thông tin 2 người.
-
-Tự động thêm role là user cho mỗi đối tượng.
-
-Tạo một hàm login nhận vào 2 tham số email và password.
-
-Yêu cầu:
-
-Nếu thông tin hợp lệ với một trong các đối tượng đã đăng ký, trả về thông tin của đối tượng đó.
-
-Nếu không, báo cho người dùng rằng “Thông tin đăng nhập không hợp lệ”.
-
-Input:
-
-const data = [];
-const dataRegister = handleRegister(
-  "Nguyen Van A",
-  "123456",
-  "nguyenvana@email.com"
-);
-const dataRegister = handleRegister(
-  "Nguyen Van B",
-  "1234567",
-  "nguyenvanb@email.com"
-);
-const dataLogin = handleLogin("nguyenvanb@email.com", "1234567");
-Output:
-
-data = [
-  {
-    name: "Nguyen Van A",
-    password: "123456",
-    email: "nguyenvana@email.com",
-    role: "user",
+var errors = {
+  name: {
+    required: "Vui lòng nhập họ tên",
+    min: "Họ tên phải từ 5 ký tự",
   },
-  {
-    name: "Nguyen Van B",
-    password: "1234567",
-    email: "nguyenvanb@email.com",
-    role: "user",
+  email: {
+    email: "Định dạng email không hợp lệ",
+    unique: "Email đã có người sử dụng",
+    required: "Vui lòng nhập địa chỉ email",
   },
-];
-dataLogin = {
-  name: "Nguyen Van B",
-  password: "1234567",
-  email: "nguyenvanb@email.com",
-  role: "user",
+  password: {
+    required: "Vui lòng nhập mật khẩu",
+    same: "Mật khẩu phải khớp với mật khẩu nhập lại",
+  },
 };
+
+function getError(field) {
+  var keys = field.split(".");
+  var currentErrors = errors[keys[0]];
+  if (!currentErrors) {
+    return "Vui lòng nhập " + keys[0];
+  }
+
+  for (var i = 1; i < keys.length; i++) {
+    var key = keys[i];
+    if (currentErrors[key]) {
+      return currentErrors[key];
+    }
+  }
+
+  return currentErrors.required || "Vui lòng nhập " + keys[0];
+}
+
+console.log(getError("name"));
