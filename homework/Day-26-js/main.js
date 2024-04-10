8ba38e1d667296322d4f1751c2cdcb422b990692
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-04-10 11:42:23 +0700

    Nộp Bài Tập Về Nhà

--- homework/Day-26-js/main.js
+++ homework/Day-26-js/main.js
@@ -2,6 +2,36 @@ document.querySelector("#show-login").addEventListener("click", function () {
   document.querySelector(".popup").classList.add("active");
 });
 
-document.querySelector(".popup .close-btn").addEventListener("click", function () {
+document
+  .querySelector(".popup .close-btn")
+  .addEventListener("click", function () {
     document.querySelector(".popup").classList.remove("active");
   });
+function myMenuFunction() {
+  var i = document.getElementById("navMenu");
+  if (i.className === "nav-menu") {
+    i.className += " responsive";
+  } else {
+    i.className = "nav-menu";
+  }
+}
+var a = document.getElementById("loginBtn");
+var b = document.getElementById("registerBtn");
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+function login() {
+  x.style.left = "4px";
+  y.style.right = "-520px";
+  a.className += " white-btn";
+  b.className = "btn";
+  x.style.opacity = 1;
+  y.style.opacity = 0;
+}
+function register() {
+  x.style.left = "-510px";
+  y.style.right = "5px";
+  a.className = "btn";
+  b.className += " white-btn";
+  x.style.opacity = 0;
+  y.style.opacity = 1;
+}
