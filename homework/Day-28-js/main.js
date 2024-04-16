793d297f3ef7c7676247bfa0799d962bc06a74e3
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-04-16 23:09:35 +0700

    Nộp bài tập về nhà

--- homework/Day-28-js/main.js
+++ homework/Day-28-js/main.js
@@ -1,12 +1,12 @@
-let slider = document.querySelector(".slider .list");
-let items = document.querySelectorAll(".slider .list .item");
-let next = document.getElementById("next");
-let prev = document.getElementById("prev");
-let dotsContainer = document.querySelector(".slider .dots");
-let dots;
+var slider = document.querySelector(".slider .list");
+var items = document.querySelectorAll(".slider .list .item");
+var next = document.getElementById("next");
+var prev = document.getElementById("prev");
+var dotsContainer = document.querySelector(".slider .dots");
+var dots;
 
-let lengthItems = items.length - 1;
-let active = 0;
+var lengthItems = items.length - 1;
+var active = 0;
 
 next.onclick = function () {
   active = active + 1 <= lengthItems ? active + 1 : 0;
@@ -18,7 +18,7 @@ prev.onclick = function () {
   reloadSlider();
 };
 
-let refreshInterval = setInterval(() => {
+var refreshInterval = setInterval(() => {
   next.click();
 }, 3000);
 
@@ -27,8 +27,8 @@ function reloadSlider() {
 
   dotsContainer.innerHTML = "";
 
-  for (let i = 0; i <= lengthItems; i++) {
-    let dot = document.createElement("li");
+  for (var i = 0; i <= lengthItems; i++) {
+    var dot = document.createElement("li");
     dot.addEventListener("click", () => {
       active = i;
       reloadSlider();
