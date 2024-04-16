abcc3ece79513c5ee54a63dd7e048fd80ffdbff2
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-04-17 00:18:45 +0700

    Nộp Bài Tập Về Nhà

--- homework/Day-28-js/main.js
+++ homework/Day-28-js/main.js
@@ -3,10 +3,10 @@ var items = document.querySelectorAll(".slider .list .item");
 var next = document.getElementById("next");
 var prev = document.getElementById("prev");
 var dotsContainer = document.querySelector(".slider .dots");
-var dots;
 
 var lengthItems = items.length - 1;
 var active = 0;
+var refreshInterval;
 
 next.onclick = function () {
   active = active + 1 <= lengthItems ? active + 1 : 0;
@@ -18,36 +18,42 @@ prev.onclick = function () {
   reloadSlider();
 };
 
-var refreshInterval = setInterval(() => {
-  next.click();
-}, 3000);
-
 function reloadSlider() {
   slider.style.left = -items[active].offsetLeft + "px";
-
-  dotsContainer.innerHTML = "";
-
-  for (var i = 0; i <= lengthItems; i++) {
-    var dot = document.createElement("li");
-    dot.addEventListener("click", () => {
-      active = i;
-      reloadSlider();
-    });
-    if (i === active) {
-      dot.classList.add("active");
-    }
-    dotsContainer.appendChild(dot);
-  }
 
   clearInterval(refreshInterval);
   refreshInterval = setInterval(() => {
     next.click();
-  }, 2500);
+  }, 3000);
 
-  dots = document.querySelectorAll(".slider .dots li");
+  updateDots();
 }
 
-reloadSlider();
+function updateDots() {
+  var dots = document.querySelectorAll(".slider .dots li");
+  dots.forEach(function (dot, index) {
+    dot.addEventListener("click", function () {
+      active = index;
+      reloadSlider();
+    });
+    if (index === active) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+}
+
+function createDots() {
+  for (var i = 0; i <= lengthItems; i++) {
+    var dot = document.createElement("li");
+    dot.classList.add("dot");
+    dotsContainer.appendChild(dot);
+  }
+  updateDots();
+}
+
+createDots();
 
 window.onresize = function (event) {
   reloadSlider();
