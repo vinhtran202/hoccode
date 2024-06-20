2ed4dd3efe9b226a2616e430db6ec3cd7e913959
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-06-20 23:42:37 +0700

    BTVN

--- homework/Day45-react/todo-app/src/App.jsx
+++ homework/Day45-react/todo-app/src/App.jsx
@@ -1,43 +1,35 @@
-import { useState, useEffect } from "react";
-import { getApiKey } from "./utils/api";
-import useTodoAPI from "./hooks/useTodoAPI";
-import TodoList from "./components/TodoList";
-import TodoForm from "./components/TodoForm";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const [apiKey, setApiKey] = useState("");
-  const { todos, loading, createTodo, updateTodo, deleteTodo } =
-    useTodoAPI(apiKey);
-
-  useEffect(() => {
-    const fetchApiKey = async () => {
-      try {
-        const key = await getApiKey("your-email@example.com");
-        setApiKey(key);
-      } catch (error) {
-        console.error("Error getting API key:", error);
-      }
-    };
-    fetchApiKey();
-  }, []);
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-4">Todo List</h1>
-      <TodoForm createTodo={createTodo} />
-      {loading ? (
-        <div className="flex justify-center">
-          <div className="spinner border-4 border-blue-500 rounded-full w-8 h-8 animate-spin"></div>
-        </div>
-      ) : (
-        <TodoList
-          todos={todos}
-          updateTodo={updateTodo}
-          deleteTodo={deleteTodo}
-        />
-      )}
-    </div>
-  );
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- homework/Day45-react/todo-app/src/main.jsx
+++ homework/Day45-react/todo-app/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { CharkraProvider, extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+import App from "./App.jsx";
+import "./index.css";
+
+export const theme = extendTheme({
+  fonts: {
+    body: "'Josefin Sans', sans-serif",
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <CharkraProvider theme={theme}>
     <App />
-  </React.StrictMode>,
-)
+  </CharkraProvider>
+);
