5375745d91f59144d736b73e05673668a4b0d396
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-07-06 23:34:49 +0700

    Nộp bt về nhà

--- homework/Day48-react/guessmynumber/src/components/Login.jsx
+++ homework/Day48-react/guessmynumber/src/components/Login.jsx
@@ -69,11 +69,11 @@ export default function Login() {
                 type="text"
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 {...register("email", {
-                  required: { value: true, message: "Vui lòng nhập Email..." },
+                  required: { value: true, message: "Please enter Email..." },
                   pattern: {
                     value:
                       /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/,
-                    message: "Email không đúng định dạng",
+                    message: "Email invalidate",
                   },
                 })}
               />
@@ -111,7 +111,7 @@ export default function Login() {
                 {...register("password", {
                   required: {
                     value: true,
-                    message: "Vui lòng nhập mật khẩu...",
+                    message: "Please enter a password...",
                   },
                   minLength: {
                     value: 4,
