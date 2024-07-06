03c02dd11d4a198994e699ba35a692cc5355c1d3
vinhtran202 <vinhtran.gtvt@gmail.com>
2024-07-06 23:16:30 +0700

    Nộp btvn

--- homework/Day48-react/guessmynumber/src/components/Login.jsx
+++ homework/Day48-react/guessmynumber/src/components/Login.jsx
@@ -28,14 +28,14 @@ export default function Login() {
     }
   };
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    <div className="flex min-h-full flex-col justify-center px-3 py-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <img
-          className="mx-auto h-36 w-auto "
+          className="mx-auto h-48 w-auto "
           src={logospace}
           alt="Your Company"
         />
-        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900 bg-white dark:bg-gray-900 text-black dark:text-white">
+        <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight  bg-white dark:bg-gray-900 text-black dark:text-white w-max ">
           Sign in to your account
         </h2>
       </div>
@@ -58,7 +58,7 @@ export default function Login() {
           <div>
             <label
               htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900 bg-white dark:bg-gray-900 text-black dark:text-white"
+              className="block text-sm font-medium leading-6  bg-white dark:bg-gray-900 text-black dark:text-white"
             >
               Email address
             </label>
@@ -129,10 +129,10 @@ export default function Login() {
 
           <div>
             <button
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="flex w-full h-16 justify-center rounded-md bg-indigo-600 px-3 py-4 text-2xl font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               disabled={status === "pending..."}
             >
-              {status === "pending..." ? "Đang xử lý" : "Đăng Nhập"}
+              {status === "pending..." ? "PENDING..." : "LOGIN"}
             </button>
           </div>
         </form>
